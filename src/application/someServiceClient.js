debfa0054c83007179f073144b3e61ff93d572d2
OlehTysovskyi <oleg.tisovsky228@gmail.com>
2025-03-20 14:47:51 +0200

    Refactor SomeServiceClient to improve flexibility and error handling - Injected mail transporter as parameter to BirthdayService for better testability - Improved error handling for failed mail transporter creation and greeting sending - Replaced hardcoded values with parameters for mail transporter configuration

--- src/application/someServiceClient.js
+++ src/application/someServiceClient.js
@@ -1,16 +1,32 @@
-import {OurDate} from "../core/ourDate";
-import {BirthdayService} from "./birthdayService";
-import {FileEmployeesRepository} from "../infrastructure/repositories/fileEmployeesRepository";
+import { OurDate } from "../core/ourDate";
+import { BirthdayService } from "./birthdayService";
+import { FileEmployeesRepository } from "../infrastructure/repositories/fileEmployeesRepository";
+import nodemailer from "nodemailer";
 
 export class SomeServiceClient {
 
     runService() {
         const filename = "employee_data.txt";
-        const service = new BirthdayService(new FileEmployeesRepository(filename));
+        const employeesRepository = new FileEmployeesRepository(filename);
+
+        // Створення транспортеру для відправки повідомлень
+        const transport = nodemailer.createTransport({
+            host: "localhost",
+            port: 25,
+        });
+
+        // Перевірка чи транспортер створено
+        if (!transport) {
+            console.error("Mailer transport not created");
+            return;
+        }
+
+        const service = new BirthdayService(employeesRepository);
+
         try {
-            service.sendGreetings(new OurDate("2008/10/08"), "localhost", 25,"sender@here.com");
+            service.sendGreetings(new OurDate("2008/10/08"), transport, "sender@here.com");
         } catch (e) {
-            console.log(e);
+            console.error("Error occurred while sending greetings:", e);
         }
     }
 }
