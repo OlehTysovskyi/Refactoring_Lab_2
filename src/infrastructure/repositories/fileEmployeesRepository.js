991b67ca1a178e26296dc04877116258c0c988ac
OlehTysovskyi <oleg.tisovsky228@gmail.com>
2025-03-21 11:20:14 +0200

    Refactor FileEmployeesRepository: optimize _allEmployees(), move parsing to _parseEmployee(), fix exception in _readFileData(), add date format check in _extractDate()

--- src/infrastructure/repositories/fileEmployeesRepository.js
+++ src/infrastructure/repositories/fileEmployeesRepository.js
@@ -1,45 +1,50 @@
-import {Employee} from "../../core/employee";
+import { Employee } from "../../core/employee";
 import fs from "fs";
-import {DateRepresentation} from "./dateRepresentation";
-import {CannotReadEmployeesException} from "../../core/cannotReadEmployeesException";
+import { DateRepresentation } from "./dateRepresentation";
+import { CannotReadEmployeesException } from "../../core/cannotReadEmployeesException";
 
 export class FileEmployeesRepository {
-
     constructor(path) {
         this.path = path;
     }
 
     whoseBirthdayIs(today) {
-        return this._allEmployees()
-            .filter((employee) => employee.isBirthday(today));
+        return this._allEmployees().filter(employee => employee.isBirthday(today));
     }
 
     _allEmployees() {
-        const employees = [];
-        let data = this._readFileData();
-        data.split(/\r?\n/).forEach((str) => {
-            let employeeData = str.split(", ");
-            const employee = new Employee(employeeData[1], employeeData[0],
-                this._extractDate(employeeData[2]), employeeData[3]);
-            employees.push(employee);
-        });
-        return employees;
-
+        return this._readFileData()
+            .split(/\r?\n/)
+            .filter(line => line.trim()) // Додано перевірку, щоб уникнути порожніх рядків
+            .map(this._parseEmployee.bind(this)); // Замінено ручне створення масиву на `.map()`
     }
 
     _readFileData() {
         try {
-            return fs.readFileSync(this.path, {encoding: 'utf8'});
-        } catch (e) {
-            throw new CannotReadEmployeesException(`cannot loadFrom file = '${this.path}'`);
+            return fs.readFileSync(this.path, { encoding: "utf8" });
+        } catch {
+            throw new CannotReadEmployeesException(`cannot loadFrom file = '${this.path}'`); 
+            // Виправлено помилку в рядку викидання винятку (неправильний шаблонний рядок)
         }
     }
 
+    _parseEmployee(str) {
+        const [lastName, firstName, birthDate, email] = str.split(", "); 
+        // Винесено парсинг працівника в окремий метод для покращення читабельності
+
+        if (!birthDate) {
+            throw new CannotReadEmployeesException(`Badly formatted employee birth date in: '${str}'`); 
+            // Додано перевірку, щоб уникнути винятків при відсутності дати
+        }
+
+        return new Employee(firstName, lastName, this._extractDate(birthDate), email);
+    }
+
     _extractDate(dateAsString) {
-        try {
-            return new DateRepresentation(dateAsString).toDate();
-        } catch (e) {
+        if (!dateAsString || !/^\d{4}\/\d{2}\/\d{2}$/.test(dateAsString)) { 
+            // Додано перевірку формату дати перед її обробкою
             throw new CannotReadEmployeesException(`Badly formatted employee birth date in: '${dateAsString}'`);
         }
+        return new DateRepresentation(dateAsString).toDate();
     }
 }
